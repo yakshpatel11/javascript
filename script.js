3531a5485838d1bab444423034d800ffdd1133c8
yakshpatel11 <yakshp1112@gmail.com>
2025-04-22 12:06:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,515 +1,62 @@
-//12345
-// 2345
-//  345
-//   45
-//    5
+// 2, 1, (1/2), (1/4),//
+// let x = 2;
+// let y = 1;
+// let z = 1 / 2;
 
-for (let i = 1; i <= 5; i++) {
-    for (let k = 1; k < i; k++) {
-        document.writeln("-");
+// for (let i = 1; i <= 4; i++) {
+//     let result = x + y * z;
+//     document.writeln(result + "<br>");
+//     x = y;
+//     y = z;
+//     // get the last digit of the result//
+// }
+//7, 10, 8, 11, 9, 12,//
+// let a = 7;
+// for (let i = 1; i <= 6; i++) {
+//     document.writeln(a + " "+ a);
+//    for (let j = 1; j <= 2; j++)  {
+//         a = a + 1;
+//     }
+//     if (i % 2 != 0) {
+//         a = a + 3;
+
+
+//     }
+// }
+// 53, 53, 40, 40, 27, 27//
+// let b = 53;
+// for (let i = 1; i <= 4; i++) {
+//     document.writeln(b + "  " + b);
+//     b = b - 13;
+
+// }
+// //  22, 21, 23, 22, 24, 23, ... What number should come next?//
+// let c = 22;
+// for (let i = 1; i <= 6; i++) {
+//     document.writeln(c + " ");
+//     c -= 1;
+//     c += 2;;
+//     if (let i == 6) {
+//         c = 23;
+//     }
+// }
+
+// 1, 2, 4, 8, 16, 32, ... what number should come next?//
+let d = 1;
+for (let i = 1; i <= 6; i++) {
+    document.writeln(d + " ");
+    d *= 2;
+    if (i == 6) {
+        d = 64;
     }
-    for (let j = 1; j <= 5; j++) {
-        document.writeln(j);
-    }
-    document.writeln("<br>");
+
 }
-
-// 1
-//2 1
-//3 2 1
-//4 3 2 1
-//5 4 3 2 1 
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = i; j >= 1; j--) {
-        document.writeln(j + " ");
-    }
-    document.writeln("<br>");
-}
-
-// 5
-//4 5
-//3 4 5
-//2 3 4 5
-//1 2 3 4 5
-
-for (let i = 5; i >= 1; i--) {
-    for (let j = i; j <= 5; j++) {
-        document.writeln(j + " ");
-    }
-    document.writeln("<br>");
-}
-
-//5
-//5 4
-//5 4 3
-//5 4 3 2
-//5 4 3 2 1
-
-for (let i = 5; i >= 1; i--) {
-    for (let j = 5; j >= i; j--) {
-        document.writeln(j + " ");
-    }
-    document.writeln("<br>");
-}
-
-//1
-//2 2
-//3 3 3
-//4 4 4 4
-//5 5 5 5 5
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= i; j++) {
-        document.writeln(i + " ");
-    }
-    document.writeln("<br>");
-}
-
-//5
-//4 4
-//3 3 3
-//2 2 2 2
-//1 1 1 1 1
-
-for (let i = 5; i >= 1; i--) {
-    for (let j = 1; j <= i; j++) {
-        document.writeln(i + " ");
-    }
-    document.writeln("<br>");
-}
-
-//*
-//* *
-//* * *
-//* * * *
-//* * * * *
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= i; j++) {
-        document.writeln("*");
-    }
-    document.writeln("<br>");
-}
-
-//* * * * *
-//* * * *
-//* * *
-//* *
-//*
-
-for (let i = 5; i >= 1; i--) {
-    for (let j = 1; j <= i; j++) {
-        document.writeln("*");
-    }
-    document.writeln("<br>");
-}
-
-//- | - | -
-//- | - |
-//- | -
-//- |
-//-
-
-for (let i = 5; i >= 1; i--) {
-    for (let j = 1; j <= i; j++) {
-
-        if (j == i) {
-            document.writeln("-");
-            document.writeln("<br>");
+ //  21, 9, 21, 11, 21, 13, 21, ... What number should come next?//
+ let e = 9;
+   for (let i = 1; i <=4; i++) {
+        document.writeln(e + " ");
+        e += 2;
+        if (i == 6) {
+            e= 21;
         }
-
-        else {
-            document.writeln("- |");
-        }
-
-    }
-}
-
-//* * * * *
-//*       *
-//* * * * *
-//*       *
-//*       *
-//*       *
-//*       *
-
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (j == 1 || j == 5 || i == 1 || i == 3) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-
-}
-
-document.writeln("<br>");
-
-// * * *
-// *    *
-// *   *
-// * * 
-//*    *
-//*     *
-//* * *
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (j == 1 || j == 5 || i == 1 || i == 3 || i == 5) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-// * * * *
-// *
-// *
-// *
-// *
-// * * * *
-
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (i == 1 || i == 5) {
-            document.writeln("*");
-        } else if (j == 1) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-// *   *
-// *      *
-// *       *
-// *        *
-// *        *
-// *       *
-// *      *
-// *    *
-// *  *      
-
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (i == 1 || i == 5) {
-            document.writeln("*");
-        } else if (j == 1 || j == 5) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-
-// * * * *
-// *
-// *
-// * * * *
-// *
-// *
-// * * * *
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (i == 1 || i == 5) {
-            document.writeln("*");
-        } else if (j == 1 || i == 5 || i == 3) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-
-// * * * *
-// *
-// *
-// * * * *
-// *
-// *
-// *
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (i == 1) {
-            document.writeln("*");
-        } else if (j == 1 || i == 3) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-
-// * * *
-// *     *
-// *
-// * * * *
-// *     *
-// *     *
-// * * *
-
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (i == 1 || i == 5 || j == 1 || j == 5) {
-            document.writeln("*");
-        } else if (j == 1 || j == 5 || i == 3) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-
-
-// *       *
-// *       *
-// *       *
-// * * * * *
-// *       *
-// *       *
-// *       *
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (j == 1 || j == 5 || i == 3) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-
-}
-
-document.writeln("<br>");
-
-// * * * * *
-//     *
-//     *
-//     *
-//     *
-// * * * * *
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 3; j <= 7; j++) {
-        if (i == 1 || i == 5) {
-            document.writeln("*");
-        } else if (j == 1 || j == 5) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-// * * * * *
-//     *
-//     *
-//     *
-// *   *
-//  * * 
-
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 3; j <= 6; j++) {
-        if (i == 1 || i == 5) {
-            document.writeln("*");
-        } else if (j == 1 || j == 6) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-// *       *
-// *     *
-// *   *
-// * *
-// *   *
-// *     *
-// *       *
-
-
-for (let i = 1; i <= 7; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (i == 5 || i == 5) {
-            document.writeln("*");
-        } else if (j == 1) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-// *
-// *
-// *
-// *
-// *
-// *
-// * * * * *
-
-for (let i = 1; i <= 7; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (i == 7) {
-            document.writeln("*");
-        } else if (j == 1) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-// *     *
-// * * * *
-// *  *  *
-// *     *
-// *     *
-// *     *
-
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (i == 2 || i == 3) {
-            document.writeln("*");
-        } else if (j == 1 || j == 5) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-// * * * * *
-// *       *
-// *       *
-// *       *
-// *       *
-// *       *
-// * * * * *
-
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (i == 1 || i == 5) {
-            document.writeln("*");
-        } else if (j == 1 || j == 5) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-
-// * * * *
-// *     *
-// *     *
-// * * * *
-// *
-// *
-// *
-
-
-
-for (let i = 1; i <= 7; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (i == 1 || i == 4) {
-            document.writeln("*");
-        } else if (j == 1 || j == 5) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-
-// * * * * *
-//     *
-//     *
-//     *
-//     *
-//     *
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 3; j <= 7; j++) {
-        if (i == 1) {
-            document.writeln("*");
-        } else if (j == 1 || j == 5) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
-
-// *       *
-// *       *
-// *       *
-// *       *
-// *       *
-// *       *
-// * * * * *
-
-
-for (let i = 1; i <= 5; i++) {
-    for (let j = 1; j <= 5; j++) {
-        if (i == 5) {
-            document.writeln("*");
-        } else if (j == 1 || j == 5) {
-            document.writeln("*");
-        } else {
-            document.writeln("&nbsp&nbsp");
-        }
-    }
-    document.writeln("<br>");
-}
-document.writeln("<br>");
-
+   }
