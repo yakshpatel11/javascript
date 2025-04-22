d4acd9ca43b27e6dc2c35aa0bf283dc62372ce62
yakshpatel11 <yakshp1112@gmail.com>
2025-04-22 12:30:04 +0530

    Add files via upload

---
+++ Operators/script (1).js
@@ -0,0 +1,45 @@
+//arithmatic operators[mathmatical opration]//
+//+,-,*,/,%[for get reminder]
+//For Example//
+document.writeln(12+2);
+document.writeln(25-5);
+document.writeln(5*8);
+document.writeln(10/5)
+
+//assignment operators //
+// use for assign to the variable //
+//For Example //
+let y = 25;
+let z = 20;
+y = y +20;
+z *= y +25
+ document.writeln(z)
+
+
+ //Relational Operators [Conditional,Comparsion] // 
+ // Use For Compare To Values //
+ //For EXamples //
+ let a =25;
+ let b =30;
+ document.writeln[a>b]
+ document.writeln[b<a]
+ document.writeln[a>=b]
+ document.writeln[a<=b]
+ document.writeln[a==b]
+ document.writeln[a!=b]
+
+
+ //Logical Operators //
+ // Use For Connect Two Or More Conditions //
+ //For Examples //
+ //&& - Conditions Must Be True //
+ //False && False=False// 
+
+ //|| One Condition Is True // 
+ //0 0=0//
+ //0 1=1//
+
+ //! Not //
+ //0=1//
+ //1=0//
+
