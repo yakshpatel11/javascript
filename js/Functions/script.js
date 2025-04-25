84b634746a21dc5f92707b4d869fd5d3e9c21763
yakshpatel11 <yakshp1112@gmail.com>
2025-04-25 11:25:41 +0530

    Add files via upload

---
+++ js/Functions/script.js
@@ -0,0 +1,39 @@
+// create a function to generate table of user given number.
+x = 5;
+prompt("Enter a number to generate its multipication table:");
+function generateTable(x) {
+
+    for (let i = 1; i <= 10; i++) {
+        document.writeln(x + " * " + i + " = " + (x * i) + "<br>");
+    }
+
+
+}
+generateTable(1);
+generateTable(2);
+generateTable(3);
+generateTable(4);
+generateTable(5);
+generateTable(6);
+generateTable(7);
+generateTable(8);
+generateTable(9);
+generateTable(10);
+
+// print a 1 to n table using a function.
+x = 5;
+prompt("Enter a number to generate its multipication table:");
+function generateTable(x) {
+
+    for (let i = 1; i <= 10; i++) {
+        document.writeln(x + " * " + i + " = " + (x * i) + "<br>");
+    }
+
+
+}
+generateTable(1);
+generateTable(2);
+generateTable(3);
+generateTable(4);
+generateTable(5);
+
